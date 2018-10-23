68328a143e4ca7d534f686003392d1883316329a
sifang_liu <601607379@qq.com>
2018-10-23 15:24:56 +0800

    ssm基础框架

---
+++ src/main/java/com/juxing/service/Impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.juxing.service.Impl;
+
+import com.juxing.service.UserService;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/10/23 14
+ * @Description:
+ */
+public class UserServiceImpl implements UserService {
+}
---
+++ src/main/java/com/juxing/service/UserService.java
@@ -0,0 +1,9 @@
+package com.juxing.service;
+
+/**
+ * @Auther: Administrator
+ * @Date: 2018/10/23 14
+ * @Description:
+ */
+public interface UserService {
+}
